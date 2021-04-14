5c40e51685beff2bccc46184cf75e9c64196b040
Ryan Chen <ryan101.chen@gmail.com>
2021-04-14 14:34:08 +0800

    refactor: rename variable

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,7 +1,7 @@
 public class TennisGame1 implements TennisGame {
 
-	private int m_score1 = 0;
-	private int m_score2 = 0;
+	private int player1ScoreTimes = 0;
+	private int player2ScoreTimes = 0;
 	private String player1Name;
 	private String player2Name;
 
@@ -12,17 +12,17 @@ public class TennisGame1 implements TennisGame {
 
 	public void wonPoint(String playerName) {
 		if (playerName == "player1")
-			m_score1 += 1;
+			player1ScoreTimes += 1;
 		else
-			m_score2 += 1;
+			player2ScoreTimes += 1;
 	}
 
 	public String getScore() {
 		String score = "";
 		int tempScore=0;
-		if (m_score1==m_score2)
+		if (player1ScoreTimes == player2ScoreTimes)
 		{
-			switch (m_score1)
+			switch (player1ScoreTimes)
 			{
 				case 0:
 					score = "Love-All";
@@ -39,9 +39,9 @@ public class TennisGame1 implements TennisGame {
 
 			}
 		}
-		else if (m_score1>=4 || m_score2>=4)
+		else if (player1ScoreTimes >=4 || player2ScoreTimes >=4)
 		{
-			int minusResult = m_score1-m_score2;
+			int minusResult = player1ScoreTimes - player2ScoreTimes;
 			if (minusResult==1) score ="Advantage player1";
 			else if (minusResult ==-1) score ="Advantage player2";
 			else if (minusResult>=2) score = "Win for player1";
@@ -51,8 +51,8 @@ public class TennisGame1 implements TennisGame {
 		{
 			for (int i=1; i<3; i++)
 			{
-				if (i==1) tempScore = m_score1;
-				else { score+="-"; tempScore = m_score2;}
+				if (i==1) tempScore = player1ScoreTimes;
+				else { score+="-"; tempScore = player2ScoreTimes;}
 				switch(tempScore)
 				{
 					case 0:
